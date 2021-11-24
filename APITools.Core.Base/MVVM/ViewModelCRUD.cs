1b7cd3126a98a33b51ba14cb7007c86ea02099b7
Gaspard Andrieu <adr.gaspard@gmail.com>
2021-11-24 21:52:59 +0100

    Suppression de la partie MVVM

--- APITools.Core.Base/MVVM/ViewModelCRUD.cs
+++
@@ -1,9 +0,0 @@
-﻿using APITools.Core.Base.DAO.Models;
-
-namespace APITools.Core.Base.MVVM
-{
-    public abstract class ViewModelCRUD<TEntity> : ViewModel where TEntity : class, IGuidResolvable, IValidatable
-    {
-        // TODO
-    }
-}
